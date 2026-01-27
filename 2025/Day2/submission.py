4ee4197ca96a7d5b83b6dc5f41dca136dbef22f2
Jacob Ennis <123218842+gandalfthegreenery@users.noreply.github.com>
2026-01-27 14:57:52 -0800

    Refactor checking_halves function and clean up code
    Refactor and simplify the checking_halves function. Remove unused functions and improve readability.

--- 2025/Day2/submission.py
+++ 2025/Day2/submission.py
@@ -1,121 +1,4 @@
-import io
 import csv
-
-
-def checking_sevenths(start,end):
-    total_sum = 0
-    if len(start)!=len(end):
-        if len(end)%7 !=0:
-            end = "9"*len(start)
-        else:
-            start = "1"
-            i=0
-            while i<len(end)-1:
-                start+="0"
-                i+=1
-    if(len(start)!=7):
-        print("length not divisible by 7")
-    else:
-        print("divisible by 7")
-        st1 = int(start[0])
-        en1 = int(end[0])
-        while st1 < en1:
-            if int(start)<=int(str(st1)*7):
-                total_sum+=int(str(st1)*7)
-            st1+=1
-        if st1==en1:
-            if int(end)>=int(str(st1)*7)>=int(start):
-                total_sum+=int(str(st1)*7)
-    return total_sum
-
-def checking_fifths(start,end):
-    total_sum = 0
-    if len(start)!=len(end):
-        if len(end)%5 !=0:
-            end = "9"*len(start)
-        else:
-            start = "1"
-            i=0
-            while i<len(end)-1:
-                start+="0"
-                i+=1
-    if(len(start)%5!=0):
-        print("length not divisible by 5")
-    else:
-        print("divisible by 5")
-        if len(start)==5:
-            st1 = int(start[0])
-            en1 = int(end[0])
-            while st1 < en1:
-                if int(start)<=int(str(st1)*5):
-                    total_sum+=int(str(st1)*5)
-                st1+=1
-            if st1==en1:
-                if int(end)>=int(str(st1)*5)>=int(start):
-                    total_sum+=int(str(st1)*5)
-        else:
-            assert(len(start)==10)
-            st1 = int(start[:2])
-            en1 = int(end[:2])
-            while st1 < en1:
-                if int(start)<=int(str(st1)*5) and start[0]!=start[1]:
-                    total_sum+=int(str(st1)*5)
-                st1+=1
-            if st1==en1:
-                if int(end)>=int(str(st1)*5)>=int(start) and start[0]!=start[1]:
-                    total_sum+=int(str(st1)*5)
-    return total_sum
-
-def checking_thirds(start,end):
-    total_sum = 0
-    if len(start)!=len(end):
-        if len(end)%3 !=0:
-            end = "9"*len(start)
-        else:
-            start = "1"
-            i=0
-            while i<len(end)-1:
-                start+="0"
-                i+=1
-    if(len(start)%3!=0):
-        print("length not divisible by 3")
-    else:
-        print("divisible by 3")
-        if len(start)==3:
-            st1 = int(start[0])
-            en1 = int(end[0])
-            while st1 < en1:
-                if int(start)<=int(str(st1)*3):
-                    total_sum+=int(str(st1)*3)
-                st1+=1
-            if st1==en1:
-                if int(end)>=int(str(st1)*3)>=int(start):
-                    total_sum+=int(str(st1)*3)
-
-        elif len(start)==6:
-            st1 = int(start[:2])
-            en1 = int(end[:2])
-            while st1 <en1:
-                if int(start)<=int(str(st1)*3) and str(st1)[0]!=str(st1)[1]:
-                    total_sum+=int(str(st1)*3)
-                st1+=1
-            if st1==en1:
-                if int(end)>=int(str(st1)*3)>=int(start) and str(st1)[0]!=str(st1)[1]:
-                    total_sum+=int(str(st1)*3)
-        else:
-            assert(len(start)==9)
-            st1 = int(start[:3])
-            en1 = int(end[:3])
-            while st1 < en1:
-                if int(start)<=int(str(st1)*3):
-                    total_sum+=int(str(st1)*3)
-                st1+=1
-
-            if st1==en1:
-                if int(end)>=int(str(st1)*3)>=int(start):
-                    total_sum+=int(str(st1)*3)
-
-    return total_sum
 
 def checking_halves(start,end):
     total_sum=0
@@ -144,7 +27,6 @@ def checking_halves(start,end):
     return total_sum
 
 s1 = 0
-
 with open("AdventofCodeDay2.txt","r") as f:
     reader= csv.reader(f)
     for row in reader:
@@ -156,9 +38,6 @@ with open("AdventofCodeDay2.txt","r") as f:
             print("start:",start)
             print("end:",end)
             s1+=checking_halves(start,end)
-            s1+=checking_thirds(start,end)
-            s1+=checking_fifths(start,end)
-            s1+=checking_sevenths(start,end)
 print(s1)
 f.close()
         
