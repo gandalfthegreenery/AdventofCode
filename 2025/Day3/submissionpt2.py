025a194065f0e7d0a4b28bd840c31ffab3be64eb
Jacob Ennis <123218842+gandalfthegreenery@users.noreply.github.com>
2026-01-27 15:04:38 -0800

    Add files via upload

--- 2025/Day3/submissionpt2.py
+++ 2025/Day3/submissionpt2.py
@@ -1,10 +1,10 @@
-
+import io
 import csv
 
 s1 = 0
 total_joltage =0
 f= open("AdventofCodeDay3.txt","r") 
-length_of_string = 2
+length_of_string = 12
 length_of_row = 101
 
 
@@ -15,7 +15,15 @@ for row in f:
         for i in locations:
             j = i
             max_ind = i
+            print("row:",row)
+            print("j:",j)
+            print("k:",k)
+            print("i:",i)
+            print("end:",end)
             while j > end:
+                print('row[j]',row[j])
+                print("max_ind",max_ind)
+                print("max_val",row[max_ind])
                 if row[j]>=row[max_ind]:
                         max_ind=j
                         locations[k] = max_ind
@@ -23,6 +31,7 @@ for row in f:
                 j-=1
             k+=1
             end = max_ind
+        print(locations)
         fin = ""
         for i in locations:
               fin+=row[i]
